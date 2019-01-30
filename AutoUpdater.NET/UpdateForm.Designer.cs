630d8037c1db5c5a1304ac04692f1502e25840f3
Sergio <sergio.itwt@gmail.com>
2019-01-30 16:20:25 +0300

    Показ формы обновления поверх остальных окон

--- AutoUpdater.NET/UpdateForm.Designer.cs
+++ AutoUpdater.NET/UpdateForm.Designer.cs
@@ -111,6 +111,7 @@ namespace AutoUpdaterDotNET
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "UpdateForm";
+            this.TopMost = true;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.UpdateForm_FormClosed);
             this.Load += new System.EventHandler(this.UpdateFormLoad);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).EndInit();
